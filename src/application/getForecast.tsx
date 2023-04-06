eaed7f1f1a7fbc694330ccf46066dfd1887d7404
TiagoFuelber <tfuelber@gmail.com>
2023-04-06 11:52:08 -0300

    Fix: Indentation

--- src/application/getForecast.tsx
+++ src/application/getForecast.tsx
@@ -1,13 +1,13 @@
-import WeatherRepository from '../infra/WeatherRepository';
+import WeatherRepository from "../infra/WeatherRepository";
 
 const getForecast = async (city: string, days: number): Promise<number> => {
-    const response = await WeatherRepository.getForecast(city, days);
+  const response = await WeatherRepository.getForecast(city, days);
 
-    if (response.cod === '200') {
-        return response.list[response.cnt - 1].weather[0].id;
-    }
+  if (response.cod === "200") {
+    return response.list[response.cnt - 1].weather[0].id;
+  }
 
-    return 0;
+  return 0;
 };
 
 export default getForecast;
